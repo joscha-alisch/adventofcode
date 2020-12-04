90e82703ace974fee8f9e8b0c6e865a9513ce364
Joscha Alisch <joscha.alisch@gmail.com>
2020-12-04 21:57:01 +0100

    Solve 2020/01 - Part 2

--- 2020/01/main.go
+++ 2020/01/main.go
@@ -12,19 +12,40 @@ func main() {
 	numbers, err := util.ReadInput("./01/input.txt")
 	util.HandleError(err)
 
-	answer := calculateAnswer(numbers)
-	if answer == 0 {
+	answer1 := calculateAnswerPart1(numbers)
+	if answer1 == 0 {
 		util.HandleError(errors.New("couldn't find answer"))
 	}
 
-	log.Info().Int64("answer", answer).Msg("Got it!")
+	log.Info().Int64("answer", answer1).Msg("Part 1")
+
+	answer2 := calculateAnswerPart2(numbers)
+	if answer2 == 0 {
+		util.HandleError(errors.New("couldn't find answer"))
+	}
+
+	log.Info().Int64("answer", answer2).Msg("Part 2")
 }
 
-func calculateAnswer(numbers []int64) int64 {
+func calculateAnswerPart1(numbers []int64) int64 {
 	for _, a := range numbers {
 		for _, b := range numbers {
 			if a+b == 2020 {
 				return a * b
+			}
+		}
+	}
+
+	return 0
+}
+
+func calculateAnswerPart2(numbers []int64) int64 {
+	for _, a := range numbers {
+		for _, b := range numbers {
+			for _, c := range numbers {
+				if a+b+c == 2020 {
+					return a * b * c
+				}
 			}
 		}
 	}
